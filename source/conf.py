603378d8c89caa0c8f50a376014aac3153135eb7
TylerMS887 <115214762+TylerMS887@users.noreply.github.com>
2023-11-28 22:49:59 +0000

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -14,7 +14,7 @@ release = '24.4'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["myst-parser", "sphinx_rtd_theme"]
+extensions = ["myst_parser", "sphinx_rtd_theme"]
 
 templates_path = ['_templates']
 exclude_patterns = []
