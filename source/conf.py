f509732d562c5152ba5d91774d42cf039af74c2f
TylerMS887 <115214762+TylerMS887@users.noreply.github.com>
2023-11-28 22:41:14 +0000

    lol idk what message to add

---
+++ source/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Embux BusyBox/Linux'
+copyright = '2023, Embux Project'
+author = 'Embux Project'
+release = '24.4'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ["myst-parser", "sphinx_rtd_theme"]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphnix_rtd_theme'
+html_static_path = ['_static']
