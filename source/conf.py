a7ccb6046fbea615d2e3d864b4f0e1652914b6df
TylerMS887 <115214762+TylerMS887@users.noreply.github.com>
2023-12-01 15:19:18 +0000

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -14,7 +14,8 @@ release = '24.4'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["myst_parser", "sphinx_rtd_theme", "sphinx_external_toc"]
+extensions = ["myst_parser", "sphinx_rtd_theme", "sphinx_external_toc", "sphinx_design"]
+myst_enable_extensions = ["colon_fence"]
 external_toc_path = "_index.yaml"  # optional, default: _toc.yml
 external_toc_exclude_missing = True  # optional, default: False
 
