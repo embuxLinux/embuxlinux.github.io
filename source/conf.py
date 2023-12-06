b331bcc9d82d6bb4f20cc75aca55173ea7744109
TylerMS887 <115214762+TylerMS887@users.noreply.github.com>
2023-12-06 19:43:00 +0000

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -7,14 +7,14 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'Embux'
-copyright = '2023, Embux Project'
+copyright = 'Embux Project'
 author = 'Embux Project'
 release = '24.4'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["myst_parser", "sphinx_rtd_theme", "sphinx_external_toc", "sphinx_design"]
+extensions = ["myst_parser", "sphinx_external_toc", "sphinx_design"]
 myst_enable_extensions = ["colon_fence"]
 external_toc_path = "_index.yaml"  # optional, default: _toc.yml
 external_toc_exclude_missing = True  # optional, default: False
@@ -22,17 +22,9 @@ external_toc_exclude_missing = True  # optional, default: False
 templates_path = ['_templates']
 exclude_patterns = []
 
-html_context = {
-  'display_github': True,
-  'github_user': 'embuxLinux',
-  'github_repo': 'embuxlinux.github.io',
-  'github_version': 'main/source/',
-  'logo_only': True,
-  'html_logo': 'embuxlogo.png',
-}
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+html_permalinks_icon = '<span>#</span>'
+html_theme = 'sphinxawesome_theme'
 html_static_path = ['_static']
