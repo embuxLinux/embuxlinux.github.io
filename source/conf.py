f024f8f34bd9644bfb02916996cd4f1cd8773440
TylerMS887 <115214762+TylerMS887@users.noreply.github.com>
2024-01-09 15:16:27 +0000

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -25,6 +25,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_permalinks_icon = '<span>#</span>'
 html_theme = 'sphinxawesome_theme'
 html_static_path = ['_static']
